737aceaf61e1f639f622de435cfc995530b75653
Hoang Nguyen <ngpbhoang1406@gmail.com>
2019-09-17 21:48:34 +0700

    Override JSQL select related.

---
+++ src/main/java/org/vgu/ocl2psql/sql/statement/select/PlainSelect.java
@@ -0,0 +1,224 @@
+package org.vgu.ocl2psql.sql.statement.select;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Objects;
+
+import net.sf.jsqlparser.expression.LongValue;
+import net.sf.jsqlparser.schema.Table;
+import net.sf.jsqlparser.statement.select.AllColumns;
+import net.sf.jsqlparser.statement.select.Join;
+
+public class PlainSelect extends net.sf.jsqlparser.statement.select.PlainSelect {
+    private LinkedList<VarSelectExpression> vars = new LinkedList<VarSelectExpression>();
+    private String correspondOCLExpression = "Hello world";
+    
+    public PlainSelect() {
+        super();
+        this.setValAsTrue();
+    }
+    
+    public void setAllColumn() {
+        this.getSelectItems().clear();
+        this.addSelectItems(new AllColumns());
+    }
+    
+    public void setValAsTrue() {
+        ValSelectExpression val = new ValSelectExpression(new LongValue(1L));
+        this.setVal(val);
+    }
+
+    public ResSelectExpression getRes() {
+        if(Objects.isNull(super.getSelectItems())) {
+            return null;
+        } else {
+            return super.getSelectItems().stream()
+            .filter(item -> item instanceof ResSelectExpression)
+            .map(ResSelectExpression.class::cast)
+            .findFirst()
+            .orElse(null);
+        }
+    }
+    
+    public ValSelectExpression getVal() {
+        if(Objects.isNull(super.getSelectItems())) {
+            return null;
+        } else {
+            return super.getSelectItems().stream()
+            .filter(item -> item instanceof ValSelectExpression)
+            .map(ValSelectExpression.class::cast)
+            .findFirst()
+            .orElse(null);
+        }
+    }
+    
+    public void setRes(ResSelectExpression res) {
+        ResSelectExpression curRes = this.getRes();
+        if(Objects.isNull(curRes)) {
+            super.addSelectItems(res);
+        } else {
+            curRes = res;
+        }
+    }
+    
+    public void setVal(ValSelectExpression val) {
+        ValSelectExpression curVal = this.getVal();
+        if(Objects.isNull(curVal)) {
+            super.addSelectItems(val);
+        } else {
+            curVal.setExpression(val.getExpression());
+        }
+    }
+
+    public void addVar(VarSelectExpression varSelectExpression) {
+        this.vars.add(varSelectExpression);
+        if(Objects.isNull(this.getSelectItems())) {
+            this.setSelectItems(new LinkedList<>());
+        }
+        this.getSelectItems().add(varSelectExpression.getRef());
+    }
+    
+    public LinkedList<VarSelectExpression> getVars() {
+        return this.vars;
+    }
+
+//    public String toStringWithDescription() {
+//        if(this.correspondOCLExpression == null || this.correspondOCLExpression.isEmpty()) {
+//            return this.toString();
+//        } else {
+//            String descriptionSchema = "\n//*** BEGIN: %1$s ***//\n%2$s\n//*** END: %1$s ***//\n";
+//            return String.format(descriptionSchema, this.correspondOCLExpression, this.toString());
+//        }
+//    }
+    
+    public String toStringWithDescription() {
+        StringBuilder sql = new StringBuilder();
+        if (super.isUseBrackets()) {
+            sql.append("(");
+            sql.append(String.format("\n//*** BEGIN: %s ***//\n", this.correspondOCLExpression));
+        } else {
+            sql.append(String.format("\n//*** BEGIN: %s ***//\n", this.correspondOCLExpression));
+        }
+        sql.append("SELECT ");
+
+        if (super.getOracleHint() != null) {
+            sql.append(super.getOracleHint()).append(" ");
+        }
+
+        if (super.getSkip() != null) {
+            sql.append(super.getSkip()).append(" ");
+        }
+
+        if (super.getFirst() != null) {
+            sql.append(super.getFirst()).append(" ");
+        }
+
+        if (super.getDistinct() != null) {
+            sql.append(super.getDistinct()).append(" ");
+        }
+        if (super.getTop() != null) {
+            sql.append(super.getTop()).append(" ");
+        }
+        if (super.getMySqlSqlNoCache()) {
+            sql.append("SQL_NO_CACHE").append(" ");
+        }
+        if (super.getMySqlSqlCalcFoundRows()) {
+            sql.append("SQL_CALC_FOUND_ROWS").append(" ");
+        }
+        sql.append(getStringList(super.getSelectItems()));
+
+        if (super.getIntoTables() != null) {
+            sql.append(" INTO ");
+            for (Iterator<Table> iter = super.getIntoTables().iterator(); iter.hasNext();) {
+                sql.append(iter.next().toString());
+                if (iter.hasNext()) {
+                    sql.append(", ");
+                }
+            }
+        }
+
+        if (super.getFromItem() != null) {
+            if(super.getFromItem() instanceof SubSelect) {
+                sql.append(" FROM ").append(((SubSelect) super.getFromItem()).toStringWithDescription());
+            } else {
+                sql.append(" FROM ").append(super.getFromItem());
+            }
+            if (super.getJoins() != null) {
+                Iterator<Join> it = super.getJoins().iterator();
+                while (it.hasNext()) {
+                    Join join = it.next();
+                    if (join.isSimple()) {
+                        sql.append(", ").append(join);
+                    } else {
+                        sql.append(" ").append(join);
+                    }
+                }
+            }
+
+            if (super.getKsqlWindow() != null) {
+                sql.append(" WINDOW ").append(super.getKsqlWindow().toString());
+            }
+            if (super.getWhere() != null) {
+                sql.append(" WHERE ").append(super.getWhere());
+            }
+            if (super.getOracleHierarchical() != null) {
+                sql.append(super.getOracleHierarchical().toString());
+            }
+            if (super.getGroupBy() != null) {
+                sql.append(" ").append(super.getGroupBy().toString());
+            }
+            if (super.getHaving() != null) {
+                sql.append(" HAVING ").append(super.getHaving());
+            }
+            sql.append(orderByToString(super.isOracleSiblings(), super.getOrderByElements()));
+            if (super.getLimit() != null) {
+                sql.append(super.getLimit());
+            }
+            if (super.getOffset() != null) {
+                sql.append(super.getOffset());
+            }
+            if (super.getFetch() != null) {
+                sql.append(super.getFetch());
+            }
+            if (isForUpdate()) {
+                sql.append(" FOR UPDATE");
+
+                if (super.getForUpdateTable() != null) {
+                    sql.append(" OF ").append(super.getForUpdateTable());
+                }
+
+                if (super.getWait() != null) {
+                    // Wait's toString will do the formatting for us
+                    sql.append(super.getWait());
+                }
+            }
+            if (super.getOptimizeFor() != null) {
+                sql.append(super.getOptimizeFor());
+            }
+        } else {
+            //without from
+            if (super.getWhere() != null) {
+                sql.append(" WHERE ").append(super.getWhere() );
+            }
+        }
+        if (super.getForXmlPath() != null) {
+            sql.append(" FOR XML PATH(").append(super.getForXmlPath()).append(")");
+        }
+        if (super.isUseBrackets()) {
+            sql.append(String.format("\n//*** END: %s ***//\n", this.correspondOCLExpression));
+            sql.append(")");
+        } else {
+            sql.append(String.format("\n//*** END: %s ***//\n", this.correspondOCLExpression));
+        }
+        return sql.toString();
+    }
+
+    public String getCorrespondOCLExpression() {
+        return correspondOCLExpression;
+    }
+
+    public void setCorrespondOCLExpression(String correspondOCLExpression) {
+        this.correspondOCLExpression = correspondOCLExpression;
+    }
+    
+}
--- src/main/java/org/vgu/ocl2psql/sql/statement/select/Select.java
+++ src/main/java/org/vgu/ocl2psql/sql/statement/select/Select.java
@@ -1,28 +1,32 @@
+/**************************************************************************
+Copyright 2019 Vietnamese-German-University
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+@author: ngpbh
+***************************************************************************/
+
+
 package org.vgu.ocl2psql.sql.statement.select;
 
 import java.util.Iterator;
 
-import net.sf.jsqlparser.expression.Alias;
-import net.sf.jsqlparser.statement.select.SelectBody;
 import net.sf.jsqlparser.statement.select.WithItem;
 
-public class SubSelect extends net.sf.jsqlparser.statement.select.SubSelect {
-
-    public SubSelect() {
-        super();
-    }
-    
-    public SubSelect(SelectBody selectBody, String alias ) {
-        super();
-        super.setSelectBody( selectBody );
-        super.setAlias( new Alias( alias ) );
-    }
+public class Select extends net.sf.jsqlparser.statement.select.Select{
 
     public String toStringWithDescription() {
         StringBuilder retval = new StringBuilder();
-        if (super.isUseBrackets()) {
-            retval.append("(");
-        }
         if (super.getWithItemsList() != null && !super.getWithItemsList().isEmpty()) {
             retval.append("WITH ");
             for (Iterator<WithItem> iter = super.getWithItemsList().iterator(); iter.hasNext();) {
@@ -40,17 +44,7 @@ public class SubSelect extends net.sf.jsqlparser.statement.select.SubSelect {
         else {
             retval.append(super.getSelectBody());
         }
-        if (super.isUseBrackets()) {
-            retval.append(")");
-        }
-
-        if (super.getAlias() != null) {
-            retval.append(super.getAlias().toString());
-        }
-        if (super.getPivot() != null) {
-            retval.append(" ").append(super.getPivot());
-        }
-
         return retval.toString();
     }
+
 }
