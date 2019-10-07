d7fe9a4e461573eb9da703be623ef51e0004e726
npbhoang <ngpbhoang1406@gmail.com>
2019-10-07 10:00:59 +0700

    Refactor (4)

--- src/main/java/org/vgu/ocl2psql/test/ocl/TesterOCL2SQLParser.java
+++ src/main/java/org/vgu/ocl2psql/test/ocl/TesterOCL2SQLParser.java
@@ -15,8 +15,7 @@ limitations under the License.
 ***************************************************************************/
 package org.vgu.ocl2psql.test.ocl;
 
-import java.io.InputStream;
-import java.util.Properties;
+import java.io.File;
 
 import org.vgu.ocl2psql.main.OCL2PSQL;
 import org.vgu.ocl2psql.ocl.exception.OclParseException;
@@ -30,35 +29,42 @@ public class TesterOCL2SQLParser {
      */
     public static void main(String[] args)
             throws Exception {
-        String resourceName = "config.properties"; // could also be a constant
-        ClassLoader loader = Thread.currentThread().getContextClassLoader();
-        Properties props = new Properties();
-        try (InputStream resourceStream = loader.getResourceAsStream(resourceName)){
-            props.load(resourceStream);
 
-            OCL2PSQL ocl2psql = new OCL2PSQL();
-            ocl2psql.setPlainUMLContextFromFile(props.getProperty("cardb.filePath"));
-            ocl2psql.setDescriptionMode(true);
-            
-//            test(ocl2psql, "self");
-//            test(ocl2psql, "self = caller");
-//            test(ocl2psql, "self.Person:name");
-//            test(ocl2psql, "self.Person:name = 'Hoang'");
-//            test(ocl2psql, "self.Person:ownedCars");
-//            test(ocl2psql, "self.Person:ownedCars->size()");
-//            test(ocl2psql, "self.Person:ownedCars->exists(c|c.Car:color = 'black')");
-//            test(ocl2psql, "Car::allInstances()->exists(c|c.Car:owners->exists(p|p = self))");
-//            test(ocl2psql, "Car::allInstances()->exists(c|c.Car:owners->exists(p|p.Person:ownedCars->size() < self.Person:ownedCars->size()))");
+        OCL2PSQL ocl2psql = new OCL2PSQL();
+        File contextModel = new File("./src/main/resources/context-model/CarPerson_context.json");
+        ocl2psql.setPlainUMLContextFromFile(contextModel.getAbsolutePath());
+        ocl2psql.setDescriptionMode(true);
         
-//            test(ocl2psql, "true");
-//            test(ocl2psql, "1");
-//            test(ocl2psql, "'Hoang'");
-            test(ocl2psql, "Car::allInstances()->collect(c|c.Car:owners->forAll(p|p.Person:name <> 'Hoang'))");
-//            test(ocl2psql, "self.Car:color");
-//            test(ocl2psql, "self.Car:owners");
-//            test(ocl2psql, "Car::allInstances()->collect(c|c.Car:color)");
-//            test(ocl2psql, "Car::allInstances()->collect(c|c.Car:owners)");
-        }
+        test(ocl2psql, "self");
+        test(ocl2psql, "self = caller");
+        test(ocl2psql, "self.Person:name");
+        test(ocl2psql, "self.Person:name = 'Hoang'");
+        test(ocl2psql, "self.Person:ownedCars");
+        test(ocl2psql, "self.Person:ownedCars->size()");
+        test(ocl2psql, "self.Person:ownedCars->exists(c|c.Car:color = 'black')");
+        test(ocl2psql, "Car::allInstances()->exists(c|c.Car:owners->exists(p|p = self))");
+        test(ocl2psql, "Car::allInstances()->exists(c|c.Car:owners->exists(p|p.Person:ownedCars->size() < self.Person:ownedCars->size()))");
+    
+        test(ocl2psql, "Car::allInstances()");
+        test(ocl2psql, "Automobile::allInstances()");
+        test(ocl2psql, "Vehicle::allInstances()");
+        test(ocl2psql, "Automobile::allInstances()->collect(au|au)");
+        test(ocl2psql, "Automobile::allInstances()->collect(au|au.Vehicle:category)");
+//            test(ocl2psql, "Automobile::allInstances()->collect(au|au.Car:color)");
+        test(ocl2psql, "Automobile::allInstances()->collect(au|au.oclIsTypeOf(Automobile))");
+        test(ocl2psql, "Automobile::allInstances()->collect(au|au.oclIsTypeOf(Car))");
+        test(ocl2psql, "Automobile::allInstances()->collect(au|au.oclIsTypeOf(Person))");
+        test(ocl2psql, "Automobile::allInstances()->collect(au|au.oclIsTypeOf(Vehicle))");
+        test(ocl2psql, "Automobile::allInstances()->collect(au|au.oclIsKindOf(Automobile))");
+        test(ocl2psql, "Automobile::allInstances()->collect(au|au.oclIsKindOf(Car))");
+        test(ocl2psql, "Automobile::allInstances()->collect(au|au.oclIsKindOf(Person))");
+        test(ocl2psql, "Automobile::allInstances()->collect(au|au.oclIsKindOf(Vehicle))");
+        test(ocl2psql, "Automobile::allInstances()->collect(au|au.oclAsType(Automobile))");
+        test(ocl2psql, "Automobile::allInstances()->collect(au|au.oclAsType(Car))");
+        test(ocl2psql, "Automobile::allInstances()->collect(au|au.oclAsType(Bus))");
+//            test(ocl2psql, "Automobile::allInstances()->collect(au|au.oclAsType(Person))");
+//            test(ocl2psql, "Automobile::allInstances()->collect(au|au.oclAsType(Vehicle))");
+        test(ocl2psql, "Automobile::allInstances()->collect(au|au.oclAsType(Car).Car:color)");
     }
 
     private static void test(OCL2PSQL ocl2psql, String oclExp) throws OclParseException {
