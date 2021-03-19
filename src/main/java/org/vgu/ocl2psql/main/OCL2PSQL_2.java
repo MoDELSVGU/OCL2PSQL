fe20a145696e44e22f68b74d57f69983b0fa047c
Hoang <ngpbhoang1406@gmail.com>
2021-03-19 18:39:27 +0700

    Remove obsolete jar and classes

--- src/main/java/org/vgu/ocl2psql/main/OCL2PSQL_2.java
+++ src/main/java/org/vgu/ocl2psql/main/OCL2PSQL_2.java
@@ -18,10 +18,7 @@ limitations under the License.
 
 package org.vgu.ocl2psql.main;
 
-import java.io.BufferedWriter;
-import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileWriter;
 import java.io.IOException;
 
 import org.json.simple.JSONArray;
@@ -30,117 +27,144 @@ import org.vgu.dm2schema.dm.DataModel;
 import org.vgu.ocl2psql.ocl.parser.Ocl2PsqlSvc;
 import org.vgu.ocl2psql.ocl.parser.simple.SimpleO2PApi;
 import org.vgu.ocl2psql.ocl.roberts.exception.OclParseException;
-import org.vgu.ocl2psql.sql.statement.select.Select;
-import org.vgu.se.ocl.parser.OCLParser;
-import org.vgu.ttc2020.model.TTCReturnModel;
 
-import com.vgu.se.jocl.expressions.OclExp;
+import com.vgu.se.jocl.expressions.Expression;
 
 import net.sf.jsqlparser.statement.Statement;
 
 public class OCL2PSQL_2 {
 
-    private Ocl2PsqlSvc ocl2PsqlSvc;
+	private Ocl2PsqlSvc ocl2PsqlSvc;
 
-    public OCL2PSQL_2() {
-        ocl2PsqlSvc = new SimpleO2PApi();
-        ocl2PsqlSvc.setDescriptionMode(false);
-    }
-    
-//    public TTCReturnModel mapOCLStringToSQLXMI(String oclExpression) {
-//        TTCReturnModel returnModel = new TTCReturnModel();
-//        final long startNanoTime = System.nanoTime();
-//        Statement statement = ocl2PsqlSvc.mapToSQL(oclExpression);
-//        final long endNanoTime = System.nanoTime();
-//        returnModel.setOcl2sqlNanoTime(endNanoTime - startNanoTime);
-//        returnModel.setEStatement(SQLParser.transform(statement));
-//        return returnModel;
-//    }
-//    
-    public Statement fromOCLXMIFileToSQLStatement(File oclXMIFile) throws IOException {
-        DataModel dm = OCLParser.extractDataModel(oclXMIFile.getAbsolutePath());
-        OclExp ocl = (OclExp) OCLParser.convertToExp(oclXMIFile.getAbsolutePath());
-        ocl2PsqlSvc.setDataModel(dm);
-        return ocl2PsqlSvc.mapToSQL(ocl);
-    }
+	public OCL2PSQL_2() {
+		ocl2PsqlSvc = new SimpleO2PApi();
+		ocl2PsqlSvc.setDescriptionMode(false);
+	}
 
-    public String mapOCLStringToSQLString(String oclExpression)
-            throws OclParseException, ParseException, IOException {
-        TTCReturnModel returnModel = new TTCReturnModel();
-        final long startNanoTime = System.nanoTime();
-        Statement statement = ocl2PsqlSvc.mapToSQL(oclExpression);
-        final long endNanoTime = System.nanoTime();
-        returnModel.setOcl2sqlNanoTime(endNanoTime - startNanoTime);
-        returnModel.setStatement(((SimpleO2PApi) ocl2PsqlSvc).m2t((Select) statement));
-        return returnModel.toString();
-    }
+//	public TTCReturnModel mapOCLStringToSQLXMI(String oclExpression) {
+//		TTCReturnModel returnModel = new TTCReturnModel();
+//		final long startNanoTime = System.nanoTime();
+//		Statement statement = ocl2PsqlSvc.mapToSQL(oclExpression);
+//		final long endNanoTime = System.nanoTime();
+//		returnModel.setOcl2sqlNanoTime(endNanoTime - startNanoTime);
+//		returnModel.setEStatement(SQLParser.transform(statement));
+//		return returnModel;
+//	}
+//
+//	public Statement fromOCLXMIFileToSQLStatement(File oclXMIFile) throws IOException {
+//		DataModel dm = OCLParser.extractDataModel(oclXMIFile.getAbsolutePath());
+//		OclExp ocl = (OclExp) OCLParser.convertToExp(oclXMIFile.getAbsolutePath());
+//		ocl2PsqlSvc.setDataModel(dm);
+//		return ocl2PsqlSvc.mapToSQL(ocl);
+//	}
+//
+//	public TTCReturnModel fromOCLXMIFileToSQLXMIStatement(File oclXMIFile) throws IOException {
+//		TTCReturnModel returnModel = new TTCReturnModel();
+//		DataModel dm = OCLParser.extractDataModel(oclXMIFile.getAbsolutePath());
+//		OclExp ocl = (OclExp) OCLParser.convertToExp(oclXMIFile.getAbsolutePath());
+//		ocl2PsqlSvc.setDataModel(dm);
+//		final long startNanoTime = System.nanoTime();
+//		Statement statement = ocl2PsqlSvc.mapToSQL(ocl);
+//		final long endNanoTime = System.nanoTime();
+//		returnModel.setOcl2sqlNanoTime(endNanoTime - startNanoTime);
+//		returnModel.setEStatement(SQLParser.transform(statement));
+//		return returnModel;
+//	}
 
-    public Statement mapOCLStringToSQLModel(String oclExpression)
-            throws OclParseException, ParseException, IOException {
-        return ocl2PsqlSvc.mapToSQL(oclExpression);
-    }
-    
-    public TTCReturnModel mapOCLXMIToSQLString(String dataModelName, String dataModel, String oclXMIExpression) throws IOException {
-        final String dirPath = System.getProperty("java.io.tmpdir");
-        TTCReturnModel returnModel = new TTCReturnModel();
-        BufferedWriter output = null;
-        File dataModelFile = null;
-        try {
-            dataModelFile = new File(dirPath.concat("//").concat(dataModelName).concat(".xmi"));
-            output = new BufferedWriter(new FileWriter(dataModelFile));
-            output.write(dataModel);
-        } catch ( IOException e ) {
-            e.printStackTrace();
-        } finally {
-          if ( output != null ) {
-            output.close();
-          }
-        }
-        File file = null;
-        try {
-            file = new File(dirPath.concat("//").concat("input.xmi"));
-            output = new BufferedWriter(new FileWriter(file));
-            output.write(oclXMIExpression);
-        } catch ( IOException e ) {
-            e.printStackTrace();
-        } finally {
-          if ( output != null ) {
-            output.close();
-          }
-        }
-        String filePath = file.getAbsolutePath();
-        DataModel dm = OCLParser.extractDataModel(filePath);
-        OclExp ocl = (OclExp) OCLParser.convertToExp(filePath);
-        ocl2PsqlSvc.setDataModel(dm);
-        final long startNanoTime = System.nanoTime();
-        Statement statement = ocl2PsqlSvc.mapToSQL(ocl);
-        final long endNanoTime = System.nanoTime();
-        returnModel.setOcl2sqlNanoTime(endNanoTime - startNanoTime);
-        returnModel.setStatement(((SimpleO2PApi) ocl2PsqlSvc).m2t((Select) statement));
-        return returnModel;
-    }
+//	public String mapOCLStringToSQLString(String oclExpression) throws OclParseException, ParseException, IOException {
+//		TTCReturnModel returnModel = new TTCReturnModel();
+//		final long startNanoTime = System.nanoTime();
+//		Statement statement = ocl2PsqlSvc.mapToSQL(oclExpression);
+//		final long endNanoTime = System.nanoTime();
+//		returnModel.setOcl2sqlNanoTime(endNanoTime - startNanoTime);
+//		returnModel.setStatement(((SimpleO2PApi) ocl2PsqlSvc).m2t((Select) statement));
+//		return returnModel.toString();
+//	}
 
-    public Boolean getDescriptionMode() {
-        return ocl2PsqlSvc.getDescriptionMode();
-    }
+	public Statement mapOCLStringToSQLStatement(String oclString)
+			throws OclParseException, ParseException, IOException {
+		return ocl2PsqlSvc.mapToSQL(oclString);
+	}
+	
+	public String mapOCLExpressionToSQLString(Expression oclExpression)
+			throws OclParseException, ParseException, IOException {
+		return mapOCLExpressionToSQLStatement(oclExpression).toString();
+	}
+	
+	public String mapOCLStringToSQLString(String oclString)
+			throws OclParseException, ParseException, IOException {
+		return mapOCLStringToSQLStatement(oclString).toString();
+	}
+	
+	public Statement mapOCLExpressionToSQLStatement(Expression oclExpression)
+			throws OclParseException, ParseException, IOException {
+		return ocl2PsqlSvc.mapToSQL(oclExpression);
+	}
 
-    public void setDescriptionMode(Boolean descriptionMode) {
-        ocl2PsqlSvc.setDescriptionMode(descriptionMode);
-    }
+//	public TTCReturnModel mapOCLXMIToSQLString(String dataModelName, String dataModel, String oclXMIExpression)
+//			throws IOException {
+//		final String dirPath = System.getProperty("java.io.tmpdir");
+//		TTCReturnModel returnModel = new TTCReturnModel();
+//		BufferedWriter output = null;
+//		File dataModelFile = null;
+//		try {
+//			dataModelFile = new File(dirPath.concat("//").concat(dataModelName).concat(".xmi"));
+//			output = new BufferedWriter(new FileWriter(dataModelFile));
+//			output.write(dataModel);
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		} finally {
+//			if (output != null) {
+//				output.close();
+//			}
+//		}
+//		File file = null;
+//		try {
+//			file = new File(dirPath.concat("//").concat("input.xmi"));
+//			output = new BufferedWriter(new FileWriter(file));
+//			output.write(oclXMIExpression);
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		} finally {
+//			if (output != null) {
+//				output.close();
+//			}
+//		}
+//		String filePath = file.getAbsolutePath();
+//		DataModel dm = OCLParser.extractDataModel(filePath);
+//		OclExp ocl = (OclExp) OCLParser.convertToExp(filePath);
+//		ocl2PsqlSvc.setDataModel(dm);
+//		final long startNanoTime = System.nanoTime();
+//		Statement statement = ocl2PsqlSvc.mapToSQL(ocl);
+//		final long endNanoTime = System.nanoTime();
+//		returnModel.setOcl2sqlNanoTime(endNanoTime - startNanoTime);
+//		returnModel.setStatement(((SimpleO2PApi) ocl2PsqlSvc).m2t((Select) statement));
+//		return returnModel;
+//	}
 
-    public void setContextualType(String varName, String varType) {
-        ocl2PsqlSvc.setContextualType(varName, varType);
-    }
-    
-    public void setDataModelFromFilePath(String filePath) throws FileNotFoundException, IOException, ParseException, Exception {
-        ocl2PsqlSvc.setDataModelFromFilePath(filePath);
-    }
-    
-    public void setDataModelFromFile(JSONArray filePath) throws FileNotFoundException, IOException, ParseException, Exception {
-        ocl2PsqlSvc.setDataModelFromFile(filePath);
-    }
-    
-    public void setDataModel(DataModel dataModel) {
-        ocl2PsqlSvc.setDataModel(dataModel);
-    }
+	public Boolean getDescriptionMode() {
+		return ocl2PsqlSvc.getDescriptionMode();
+	}
+
+	public void setDescriptionMode(Boolean descriptionMode) {
+		ocl2PsqlSvc.setDescriptionMode(descriptionMode);
+	}
+
+	public void setContextualType(String varName, String varType) {
+		ocl2PsqlSvc.setContextualType(varName, varType);
+	}
+
+	public void setDataModelFromFilePath(String filePath)
+			throws FileNotFoundException, IOException, ParseException, Exception {
+		ocl2PsqlSvc.setDataModelFromFilePath(filePath);
+	}
+
+	public void setDataModelFromFile(JSONArray filePath)
+			throws FileNotFoundException, IOException, ParseException, Exception {
+		ocl2PsqlSvc.setDataModelFromFile(filePath);
+	}
+
+	public void setDataModel(DataModel dataModel) {
+		ocl2PsqlSvc.setDataModel(dataModel);
+	}
 }
--- src/test/java/org/vgu/se/ocl/dm/ParsingFromXMITest.java
+++ src/test/java/org/vgu/se/ocl/dm/ParsingFromXMITest.java
@@ -25,51 +25,50 @@ import java.util.List;
 import org.vgu.dm2schema.dm.DataModel;
 import org.vgu.ocl2psql.ocl.parser.Ocl2PsqlSvc;
 import org.vgu.ocl2psql.ocl.parser.simple.SimpleO2PApi;
-import org.vgu.se.ocl.parser.OCLParser;
 
 import com.vgu.se.jocl.expressions.OclExp;
 
 public abstract class ParsingFromXMITest {
 
     public static void main(String[] args) {
-        List<String> filePaths = Arrays.asList(
-            "C:\\Users\\ngpbh\\eclipse-workspace\\org.vgu.se.ocl\\model\\Stage0Challenge1.xmi",
-            "C:\\Users\\ngpbh\\eclipse-workspace\\org.vgu.se.ocl\\model\\Stage0Challenge2.xmi",
-            "C:\\Users\\ngpbh\\eclipse-workspace\\org.vgu.se.ocl\\model\\Stage0Challenge3.xmi",
-            "C:\\Users\\ngpbh\\eclipse-workspace\\org.vgu.se.ocl\\model\\Stage1Challenge1.xmi",
-            "C:\\Users\\ngpbh\\eclipse-workspace\\org.vgu.se.ocl\\model\\Stage1Challenge2.xmi",
-            "C:\\Users\\ngpbh\\eclipse-workspace\\org.vgu.se.ocl\\model\\Stage1Challenge3.xmi",
-            "C:\\Users\\ngpbh\\eclipse-workspace\\org.vgu.se.ocl\\model\\Stage2Challenge1.xmi",
-            "C:\\Users\\ngpbh\\eclipse-workspace\\org.vgu.se.ocl\\model\\Stage3Challenge1.xmi",
-            "C:\\Users\\ngpbh\\eclipse-workspace\\org.vgu.se.ocl\\model\\Stage3Challenge2.xmi",
-            "C:\\Users\\ngpbh\\eclipse-workspace\\org.vgu.se.ocl\\model\\Stage4Challenge1.xmi",
-            "C:\\Users\\ngpbh\\eclipse-workspace\\org.vgu.se.ocl\\model\\Stage4Challenge2.xmi",
-            "C:\\Users\\ngpbh\\eclipse-workspace\\org.vgu.se.ocl\\model\\Stage4Challenge3.xmi",
-            "C:\\Users\\ngpbh\\eclipse-workspace\\org.vgu.se.ocl\\model\\Stage5Challenge1.xmi",
-            "C:\\Users\\ngpbh\\eclipse-workspace\\org.vgu.se.ocl\\model\\Stage5Challenge2.xmi",
-            "C:\\Users\\ngpbh\\eclipse-workspace\\org.vgu.se.ocl\\model\\Stage6Challenge1.xmi",
-            "C:\\Users\\ngpbh\\eclipse-workspace\\org.vgu.se.ocl\\model\\Stage6Challenge2.xmi",
-            "C:\\Users\\ngpbh\\eclipse-workspace\\org.vgu.se.ocl\\model\\Stage7Challenge1.xmi",
-            "C:\\Users\\ngpbh\\eclipse-workspace\\org.vgu.se.ocl\\model\\Stage7Challenge2.xmi",
-            "C:\\Users\\ngpbh\\eclipse-workspace\\org.vgu.se.ocl\\model\\Stage7Challenge3.xmi",
-            "C:\\Users\\ngpbh\\eclipse-workspace\\org.vgu.se.ocl\\model\\Stage7Challenge4.xmi",
-            "C:\\Users\\ngpbh\\eclipse-workspace\\org.vgu.se.ocl\\model\\Stage8Challenge1.xmi",
-            "C:\\Users\\ngpbh\\eclipse-workspace\\org.vgu.se.ocl\\model\\Stage8Challenge2.xmi",
-            "C:\\Users\\ngpbh\\eclipse-workspace\\org.vgu.se.ocl\\model\\Stage8Challenge3.xmi",
-            "C:\\Users\\ngpbh\\eclipse-workspace\\org.vgu.se.ocl\\model\\Stage8Challenge4.xmi",
-            "C:\\Users\\ngpbh\\eclipse-workspace\\org.vgu.se.ocl\\model\\Stage9Challenge1.xmi",
-            "C:\\Users\\ngpbh\\eclipse-workspace\\org.vgu.se.ocl\\model\\Stage9Challenge2.xmi",
-            "C:\\Users\\ngpbh\\eclipse-workspace\\org.vgu.se.ocl\\model\\Stage9Challenge3.xmi",
-            "C:\\Users\\ngpbh\\eclipse-workspace\\org.vgu.se.ocl\\model\\Stage9Challenge4.xmi");
-        for (String filePath : filePaths) {
-            DataModel dm = OCLParser.extractDataModel(filePath);
-            OclExp ocl = (OclExp) OCLParser.convertToExp(filePath);
-            Ocl2PsqlSvc simpleO2P = new SimpleO2PApi();
-            simpleO2P.setDataModel(dm);
-            simpleO2P.setDescriptionMode(true);
-            String finalStatementWithDescription = simpleO2P.mapToString(ocl);
-            System.out.println(finalStatementWithDescription);
-        }
+//        List<String> filePaths = Arrays.asList(
+//            "C:\\Users\\ngpbh\\eclipse-workspace\\org.vgu.se.ocl\\model\\Stage0Challenge1.xmi",
+//            "C:\\Users\\ngpbh\\eclipse-workspace\\org.vgu.se.ocl\\model\\Stage0Challenge2.xmi",
+//            "C:\\Users\\ngpbh\\eclipse-workspace\\org.vgu.se.ocl\\model\\Stage0Challenge3.xmi",
+//            "C:\\Users\\ngpbh\\eclipse-workspace\\org.vgu.se.ocl\\model\\Stage1Challenge1.xmi",
+//            "C:\\Users\\ngpbh\\eclipse-workspace\\org.vgu.se.ocl\\model\\Stage1Challenge2.xmi",
+//            "C:\\Users\\ngpbh\\eclipse-workspace\\org.vgu.se.ocl\\model\\Stage1Challenge3.xmi",
+//            "C:\\Users\\ngpbh\\eclipse-workspace\\org.vgu.se.ocl\\model\\Stage2Challenge1.xmi",
+//            "C:\\Users\\ngpbh\\eclipse-workspace\\org.vgu.se.ocl\\model\\Stage3Challenge1.xmi",
+//            "C:\\Users\\ngpbh\\eclipse-workspace\\org.vgu.se.ocl\\model\\Stage3Challenge2.xmi",
+//            "C:\\Users\\ngpbh\\eclipse-workspace\\org.vgu.se.ocl\\model\\Stage4Challenge1.xmi",
+//            "C:\\Users\\ngpbh\\eclipse-workspace\\org.vgu.se.ocl\\model\\Stage4Challenge2.xmi",
+//            "C:\\Users\\ngpbh\\eclipse-workspace\\org.vgu.se.ocl\\model\\Stage4Challenge3.xmi",
+//            "C:\\Users\\ngpbh\\eclipse-workspace\\org.vgu.se.ocl\\model\\Stage5Challenge1.xmi",
+//            "C:\\Users\\ngpbh\\eclipse-workspace\\org.vgu.se.ocl\\model\\Stage5Challenge2.xmi",
+//            "C:\\Users\\ngpbh\\eclipse-workspace\\org.vgu.se.ocl\\model\\Stage6Challenge1.xmi",
+//            "C:\\Users\\ngpbh\\eclipse-workspace\\org.vgu.se.ocl\\model\\Stage6Challenge2.xmi",
+//            "C:\\Users\\ngpbh\\eclipse-workspace\\org.vgu.se.ocl\\model\\Stage7Challenge1.xmi",
+//            "C:\\Users\\ngpbh\\eclipse-workspace\\org.vgu.se.ocl\\model\\Stage7Challenge2.xmi",
+//            "C:\\Users\\ngpbh\\eclipse-workspace\\org.vgu.se.ocl\\model\\Stage7Challenge3.xmi",
+//            "C:\\Users\\ngpbh\\eclipse-workspace\\org.vgu.se.ocl\\model\\Stage7Challenge4.xmi",
+//            "C:\\Users\\ngpbh\\eclipse-workspace\\org.vgu.se.ocl\\model\\Stage8Challenge1.xmi",
+//            "C:\\Users\\ngpbh\\eclipse-workspace\\org.vgu.se.ocl\\model\\Stage8Challenge2.xmi",
+//            "C:\\Users\\ngpbh\\eclipse-workspace\\org.vgu.se.ocl\\model\\Stage8Challenge3.xmi",
+//            "C:\\Users\\ngpbh\\eclipse-workspace\\org.vgu.se.ocl\\model\\Stage8Challenge4.xmi",
+//            "C:\\Users\\ngpbh\\eclipse-workspace\\org.vgu.se.ocl\\model\\Stage9Challenge1.xmi",
+//            "C:\\Users\\ngpbh\\eclipse-workspace\\org.vgu.se.ocl\\model\\Stage9Challenge2.xmi",
+//            "C:\\Users\\ngpbh\\eclipse-workspace\\org.vgu.se.ocl\\model\\Stage9Challenge3.xmi",
+//            "C:\\Users\\ngpbh\\eclipse-workspace\\org.vgu.se.ocl\\model\\Stage9Challenge4.xmi");
+//        for (String filePath : filePaths) {
+//            DataModel dm = OCLParser.extractDataModel(filePath);
+//            OclExp ocl = (OclExp) OCLParser.convertToExp(filePath);
+//            Ocl2PsqlSvc simpleO2P = new SimpleO2PApi();
+//            simpleO2P.setDataModel(dm);
+//            simpleO2P.setDescriptionMode(true);
+//            String finalStatementWithDescription = simpleO2P.mapToString(ocl);
+//            System.out.println(finalStatementWithDescription);
+//        }
     }
 
 }
